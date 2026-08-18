060272448f8b79af21263e679a4c1eeb480e867c
felipemattos177 <161468507+felipemattos177@users.noreply.github.com>
2026-08-17 21:40:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -860,7 +860,7 @@ async function enviarParaWhatsApp() {
             // Abre o aplicativo/janela do WhatsApp
             window.open(`https://wa.me/${numeroLimpo}?text=${encodeURIComponent(textoPedido)}`, '_blank');
 
-        }, 5000);
+        }, 15000);
 
     } catch (erro) {
         mostrarAviso("Falha de comunicação ao tentar enviar seu pedido.", "Erro de Conexão");
